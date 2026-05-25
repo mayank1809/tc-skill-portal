47d0675f7ba34a6640a1993f8f58cfbb0900c8cd
Mayank Singh <91192266+mayank1809@users.noreply.github.com>
2026-05-25 13:48:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,28 @@ st.set_page_config(
     page_title="TC Skill Evaluation",
     layout="wide"
 )
+hide_streamlit_style = """
+    <style>
 
+    #MainMenu {
+        visibility: hidden;
+    }
+
+    header {
+        visibility: hidden;
+    }
+
+    footer {
+        visibility: hidden;
+    }
+
+    </style>
+"""
+
+st.markdown(
+    hide_streamlit_style,
+    unsafe_allow_html=True
+)
 # ---------------------------------------------------
 # SESSION STATE INITIALIZATION
 # ---------------------------------------------------
